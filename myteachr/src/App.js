918665b569d9509a33e9b8b54531576ffcc3decc
Davis Tumuhaise <tumuhaise@gmail.com>
2023-09-27 13:27:40 +0300

    add images

--- myteachr/src/App.js
+++ myteachr/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Teachers from "./components/Teachers";
-import Store from "./components/Store";
+// import Store from "./components/Store";
 import List from "./components/List";
 import Footer from "./components/Footer";
 import Dashboard from "./components/TeacherDashboard";
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/teachers" element={<Teachers />} />
-        <Route path="/store" element={<Store />} />
+        {/* <Route path="/store" element={<Store />} /> */}
         <Route path="/profile" element={<Dashboard />} />
         <Route path="/events" element={<Events />} />
        
